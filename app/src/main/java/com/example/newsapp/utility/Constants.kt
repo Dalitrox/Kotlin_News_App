9becaa7a360a6ed7cce9f16bb2a28c60581a1264
Dalitrox <79969896+Dalitrox@users.noreply.github.com>
2024-02-22 00:42:04 +0100

    Update Constants.kt
    Remove API key to template information message

--- app/src/main/java/com/example/newsapp/utility/Constants.kt
+++ app/src/main/java/com/example/newsapp/utility/Constants.kt
@@ -3,7 +3,7 @@ package com.example.newsapp.utility
 object Constants {
     const val USER_SETTINGS = "userSettings"
     const val APP_ENTRY = "appEntry"
-    const val API_KEY = "b5e5be91ee63456d984ab4910a99ce4d"
+    const val API_KEY = "Your API key"
     const val BASE_URL = "https://newsapi.org/v2/"
     val DATABASE_NAME = "news_database"
 }
